0fb26a6d898da24f6fb3e9240a50d699688626a1
jeronimoifpe <jcm1@discente.ifpe.edu.br>
2024-12-05 01:50:25 -0300

    Criacao dos controladores

---
+++ src/java/com/ifpe/controllers/MercadinhoController.java
@@ -0,0 +1,171 @@
+package com.ifpe.controllers;
+
+import com.ifpe.model.dao.ManagerDao;
+import com.ifpe.model.negocio.LoteProduto;
+import com.ifpe.model.negocio.Mercadinho;
+import java.util.List;
+import java.util.regex.Pattern;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class MercadinhoController {
+
+    private Mercadinho mercadinhoCadastro;
+    private Mercadinho selection = new Mercadinho();
+    private Mercadinho mercadinhoLogado;
+    private String modalType;
+
+    @PostConstruct
+    public void init() {
+        this.mercadinhoCadastro = new Mercadinho();
+        this.modalType = "create";
+    }
+
+    public Mercadinho getMercadinhoLogado() {
+        mercadinhoLogado = (Mercadinho) FacesContext.getCurrentInstance()
+                .getExternalContext().getSessionMap().get("mercadinhoLogado");
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("mercadinhoLogado", mercadinhoLogado);
+        return mercadinhoLogado;
+    }
+
+    public void inserir(String confirma) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        String cnpjRegex = "\\d{14}";
+        String contatoRegex = "\\d{10,11}";
+
+        boolean valid = true;
+
+        if (!mercadinhoCadastro.getSenha().equals(confirma)) {
+            context.addMessage("formCadMercadinho:senha", new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Senhas não conferem", "As senhas digitadas são diferentes."));
+            valid = false;
+        }
+
+        if (!Pattern.matches(cnpjRegex, mercadinhoCadastro.getCnpj())) {
+            context.addMessage("formCadMercadinho:cnpj", new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "CNPJ inválido", "O CNPJ deve conter 14 dígitos."));
+            valid = false;
+        }
+
+        if (!Pattern.matches(contatoRegex, mercadinhoCadastro.getContato())) {
+            context.addMessage("formCadMercadinho:contato", new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Contato inválido", "O contato deve conter 10 ou 11 dígitos."));
+            valid = false;
+        }
+
+        if (!valid) {
+            return;
+        }
+
+        // Verificar duplicidade de Login
+        if (!ManagerDao.getCurrentInstance()
+                .readAll("select m from Mercadinho m where m.login='" + mercadinhoCadastro.getLogin() + "'", Mercadinho.class)
+                .isEmpty()) {
+            context.addMessage("formCadMercadinho:login", new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Login já existe", "Tente outro login."));
+            return;
+        }
+
+        // Verificar duplicidade de CNPJ
+        if (!ManagerDao.getCurrentInstance()
+                .readAll("select m from Mercadinho m where m.cnpj='" + mercadinhoCadastro.getCnpj() + "'", Mercadinho.class)
+                .isEmpty()) {
+            context.addMessage("formCadMercadinho:cnpj", new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "CNPJ já existe", "Tente outro CNPJ."));
+            return;
+        }
+
+        // Inserir o mercadinho
+        ManagerDao.getCurrentInstance().insert(this.mercadinhoCadastro);
+        this.mercadinhoCadastro = new Mercadinho();
+
+        context.addMessage(null, new FacesMessage("Sucesso", "Mercadinho cadastrado com sucesso!"));
+    }
+
+    public void alterar() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            ManagerDao.getCurrentInstance().update(selection);
+            context.addMessage(null, new FacesMessage("Sucesso", "Mercadinho atualizado com sucesso!"));
+        } catch (Exception e) {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Erro ao atualizar mercadinho."));
+        }
+    }
+
+    public void deletar() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            ManagerDao.getCurrentInstance().delete(selection);
+            selection = new Mercadinho();
+            context.addMessage(null, new FacesMessage("Sucesso", "Mercadinho deletado com sucesso!"));
+        } catch (Exception e) {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Erro ao deletar mercadinho."));
+        }
+    }
+
+    public List<Mercadinho> readMercadinhos() {
+        return ManagerDao.getCurrentInstance().readAll("select m from Mercadinho m", Mercadinho.class);
+    }
+    
+    public Mercadinho buscarMercadinho(int p) {
+        Mercadinho mercadinho = (Mercadinho) ManagerDao.getCurrentInstance().readById(p, Mercadinho.class);
+        return mercadinho;
+    }
+
+    // Getters e Setters
+    public Mercadinho getMercadinhoCadastro() {
+        return mercadinhoCadastro;
+    }
+
+    public void setMercadinhoCadastro(Mercadinho mercadinhoCadastro) {
+        this.mercadinhoCadastro = mercadinhoCadastro;
+    }
+
+    public Mercadinho getSelection() {
+        return selection;
+    }
+
+    public void setSelection(Mercadinho selection) {
+        this.selection = selection;
+    }
+
+    public String getModalType() {
+        return modalType;
+    }
+
+    public void setModalType(String modalType) {
+        this.modalType = modalType;
+    }
+    
+    public void alterarSenha(String senha, String novaSenha, String confirma) {
+
+        //código para recuperar qualquer atributo na sessão
+        Mercadinho mLogado = selection;
+
+        if (!mLogado.getSenha().equals(senha)) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("A senha digitada está incorreta. "
+                            + "Por favor, tente novamente"));
+            return;
+        }
+
+        if (!novaSenha.equals(confirma)) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("A nova senha não bate com a confirmação. "
+                            + "Por favor, tente novamente"));
+            return;
+        }
+
+        mLogado.setSenha(novaSenha);
+
+        ManagerDao.getCurrentInstance().update(mLogado);
+
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Senha alterada com sucesso!"));
+    }
+}
---
+++ src/java/com/ifpe/controllers/ONGController.java
@@ -0,0 +1,146 @@
+package com.ifpe.controllers;
+
+import com.ifpe.model.dao.ManagerDao;
+import com.ifpe.model.negocio.LoteProduto;
+import com.ifpe.model.negocio.ONG;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean(name = "ongController")  // Certifique-se de que o nome está correto
+@SessionScoped
+public class ONGController {
+
+    private ONG ongCadastro;
+    private ONG selection = new ONG();
+    private ONG ongLogado = new ONG();
+    private String modalType;
+    private int idMercadinho;
+
+    @PostConstruct
+    public void init() {
+        this.ongCadastro = new ONG();
+        this.modalType = "create";
+    }
+
+    public ONG getOngLogada() {
+        ongLogado = ((LoginController) ((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true)).getAttribute("loginController")).getOngLogada();
+        return this.ongLogado;
+    }
+
+    public void inserir(String confirma) {
+        
+        FacesContext context = FacesContext.getCurrentInstance();
+        String contatoRegex = "\\d{10,11}";
+        
+        boolean valid = true;
+        
+        
+        List<ONG> u = ManagerDao.getCurrentInstance()
+                .readAll("select o from ONG o where o.login='" + ongCadastro.getLogin() + "'", ONG.class);
+        
+        if (!Pattern.matches(contatoRegex, ongCadastro.getContato())) {
+            context.addMessage("formCadOng:contato", new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Contato inválido", "O contato deve conter 10 ou 11 dígitos."));
+            valid = false;
+        }
+
+        if (u.isEmpty()) {
+            if (!this.ongCadastro.getSenha().equals("") && !confirma.equals("")) {
+                if (!this.ongCadastro.getSenha().equals(confirma)) {
+                    FacesContext.getCurrentInstance().addMessage("formCadONG:pswSenha",
+                            new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro Severo", "A senha e a confirmação não batem!"));
+                    return;
+                }
+
+                ManagerDao.getCurrentInstance().insert(this.ongCadastro);
+                this.ongCadastro = new ONG();
+
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage("ONG cadastrada com sucesso!"));
+            } else {
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_ERROR, "A senha não pode ficar em branco!", "Crie uma senha válida."));
+            }
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Login de ONG já existe!", "Tente outro login"));
+        }
+    }
+
+    public List<ONG> readONGs() {
+        return ManagerDao.getCurrentInstance().readAll("select o from ONG o", ONG.class);
+    }
+
+    public ONG getOngCadastro() {
+        return ongCadastro;
+    }
+
+    public void setOngCadastro(ONG ongCadastro) {
+        this.ongCadastro = ongCadastro;
+    }
+
+    public ONG getSelection() {
+        return selection;
+    }
+
+    public void setSelection(ONG selection) {
+        this.selection = selection;
+    }
+
+    public void alterar() {
+        ManagerDao.getCurrentInstance().update(this.selection);
+
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "ONG atualizada com sucesso"));
+
+    }
+
+    public void deletar() {
+        ManagerDao.getCurrentInstance().delete(this.selection);
+
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Sucesso!", "ONG deletada"));
+    }
+    
+    public void alterarSenha(String senha, String novaSenha, String confirma) {
+
+        //código para recuperar qualquer atributo na sessão
+        ONG oLogado = selection;
+
+        if (!oLogado.getSenha().equals(senha)) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("A senha digitada está incorreta. "
+                            + "Por favor, tente novamente"));
+            return;
+        }
+
+        if (!novaSenha.equals(confirma)) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("A nova senha não bate com a confirmação. "
+                            + "Por favor, tente novamente"));
+            return;
+        }
+
+        oLogado.setSenha(novaSenha);
+
+        ManagerDao.getCurrentInstance().update(oLogado);
+
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Senha alterada com sucesso!"));
+    }
+    
+    public int getIdMercadinho(){
+        return idMercadinho;
+    }
+    
+    public void setIdMercadinho(LoteProduto p){
+        idMercadinho = p.getMercadinho().getId();
+    }
+}
